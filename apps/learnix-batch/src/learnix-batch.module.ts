294cba127d1c071aeedeec79e027ae0a51922ec4
Shukhratjon <shukhratjon0717@gmail.com>
2024-12-21 00:33:31 +0900

    feat: Integrate ENV Variables

--- apps/learnix-batch/src/learnix-batch.module.ts
+++ apps/learnix-batch/src/learnix-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LearnixBatchController } from './learnix-batch.controller';
 import { LearnixBatchService } from './learnix-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [LearnixBatchController],
   providers: [LearnixBatchService],
 })
--- apps/learnix-batch/src/main.ts
+++ apps/learnix-batch/src/main.ts
@@ -3,6 +3,6 @@ import { LearnixBatchModule } from './learnix-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LearnixBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3001);
 }
 bootstrap();
