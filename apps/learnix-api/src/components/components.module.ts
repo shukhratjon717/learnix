c385c14dd2c4c1b649359a2c3b9a632cc85cda74
Shukhratjon <shukhratjon0717@gmail.com>
2025-01-14 15:40:11 +0900

    feat: creation of component modules

--- apps/learnix-api/src/components/components.module.ts
+++ apps/learnix-api/src/components/components.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
-import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
+import { NoticeModule } from './notice/notice.module';
+import { FaqModule } from './faq/faq.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [MemberModule, CourseModule]
+	imports: [
+		MemberModule,
+		AuthModule,
+		LessonModule,
+		BoardArticleModule,
+		CommentModule,
+		LikeModule,
+		ViewModule,
+		FollowModule,
+		NoticeModule,
+		FaqModule,
+		NotificationModule,
+	],
 })
 export class ComponentsModule {}
